3f3b0f8396a82777ab53f391d19bffeffb6884a5
Davron <davron1104@yandex.com>
2023-01-18 23:43:00 +0500

    second

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const bigInt = 123456789022222222222222222222222222223333333333n;
+
+const sameBigInt = BigInt(123456789022222222222222222222222222223333333333);
+
+// console.log(typeof(bigInt));
+// console.log(typeof(sameBigInt));
+
+let bigint = 5n;
+let number = 2;
+
+console.log(bigint + BigInt(number));
+console.log(Number(bigint) + number);
